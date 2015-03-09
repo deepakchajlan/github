ce9b2db4a560ab28584ff839f998681974e931b5
bhanu.prakash@magnoninternational.com <bhanu.prakash@magnoninternational.com>
2015-03-09 15:55:22 +0530

    bb
    test

---
+++ business_banking.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Business Banking</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/colorbox.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/main.css">
+<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]--> 
+<!-- Add your site or application content here -->
+<div class="wrapper">
+   <div class="container video-section">
+      <div class="content-left business-banking col-bb-10 pull-left"><img src="img/business-banking-poster.jpg" alt="Business Banking" width="1424" height="1080"></div>
+      <div class="sidebar col-bb-2 pull-left">
+         <div id="flyout-toggle" class="nav-icon pull-right"><img src="img/nav-icon.jpg" width="175" height="134" alt="nav"></div>
+         <div class="flyout-nav animated">
+            <div id="flyout-toggle-close" class="flyout-close"><span>Menu</span></div>
+            <ul class="menu">
+               <li><a href="index.php" class="menu-home">Home</a></li>
+               <li class="active"><a href="business_banking.php" class="menu-bb">Business Banking</a></li>
+               <li><a href="rise.php" class="menu-rise">Rise</a></li>
+               <li><a href="events.php" class="menu-events">Events</a></li>
+               <li><a href="bulls_eye.php" class="menu-bullseye">Bull's Eye</a></li>
+            </ul>
+         </div>
+         <div class="sidebar-inner">
+            <div class="content">
+               <h3>Business Banking</h3>
+               <p>Growth in business is about taking advantage of opportunities as you spot them. Business Banking from Emirates NBD provides comprehensive business banking solutions to small and medium enterprises in the region, that cater to your need. Businesses can now confidently explore more opportunities with the customised products & services, right advice, reach, technology and trade solutions to help their business grow further.</p>
+               <h4>Click here to view more :</h4>
+               <ol>
+                  <li><a href="img/pdf/eng/1.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li><a href="img/pdf/ar/1.jpg" class="group2">Business Solutions (Arabic)</a></li>
+                  <li><a href="img/pdf/market/1.jpg" class="group3">Market Share & Credentials</a></li>
+                  <li class="hidden"><a href="img/pdf/eng/2.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/eng/3.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/eng/4.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/eng/5.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/eng/6.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/eng/7.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/eng/8.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/eng/9.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/eng/10.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/eng/11.jpg" class="group1">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/ar/2.jpg" class="group2">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/ar/3.jpg" class="group2">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/ar/4.jpg" class="group2">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/ar/5.jpg" class="group2">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/ar/6.jpg" class="group2">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/ar/7.jpg" class="group2">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/ar/8.jpg" class="group2">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/ar/9.jpg" class="group2">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/ar/10.jpg" class="group2">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/ar/11.jpg" class="group2">Business Solutions (English)</a></li>
+                  <li class="hidden"><a href="img/pdf/market/2.jpg" class="group3">Market Share & Credentials</a></li>
+               </ol>
+               <h4> Services offered:</h4>
+               <ul>
+                  <li>Dedicated Relationship Managers</li>
+                  <li>Online Banking 24/7 with smartBUSINESS</li>
+                  <li>Cash Management and Working Capital Facilities </li>
+                  <li>Trade Finance Solutions</li>
+                  <li>Preferred Pricing </li>
+                  <li>Customised Wealth and FX solutions</li>
+               </ul>
+               <div> <img src="img/business-banking-icon.png" alt="business-banking-icon" class="business-banking-icon"></div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> 
+<script src="js/plugins.js"></script> 
+<script src="js/jquery.modern-ticker.min.js" type="text/javascript"></script> 
+<script src="js/jquery.colorbox.js"></script> 
+<script src="js/owl.carousel.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
